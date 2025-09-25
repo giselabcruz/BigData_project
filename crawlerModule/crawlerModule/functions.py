8976cc46e425ec79f6c119fb72e2e844d5d293fc
Coco-os <as.gonzalezhz@gmail.com>
2025-09-25 15:15:07 +0100

    Fixed edge cases
    Edge cases on link generation for numbers 0 and under taken out.

--- crawlerModule/crawlerModule/functions.py
+++ crawlerModule/crawlerModule/functions.py
@@ -1,9 +1,13 @@
-def link_generator(id):
+def link_generator(id: int | str) -> str:
     if isinstance(id, bool):
         raise TypeError("id must be an int or a numeric string (only digits)")
+    if id == 0:
+        raise TypeError("Id must be a positive integer greater than zero")
     if isinstance(id, int):
+        if id < 0:
+            raise TypeError("Id must be a positive integer greater than zero")
         iid = id
-    elif isinstance(id, str) and id.isdigit():
+    elif isinstance(id, str) and id.isdigit():  # type: ignore
         iid = int(id)
     else:
         raise TypeError("id must be an int or a numeric string (only digits)")
